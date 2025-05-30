62cfe6ccee09510f7b83e8ee9e07be23058e4f54
aleksander wajs <aleksander.wajs1606@gmail.com>
2025-05-30 15:07:44 +0200

    dodanie SFML

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <vector>
+#include "Enemy.h"
+#include <SFML/Graphics.hpp>
+
+vector<Vector2f>
 
 int main()
 {
     std::cout<<"Test";
+    std::vector<std::unique_ptr<Enemy>> enemies;
+    enemies.push_back(std::make_unique<BasicEnemy>(path));
+
 }
 //
 // Created by CP on 27.05.2025.
